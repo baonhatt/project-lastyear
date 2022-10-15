671b9063749506ed696828cb7809722f1cbd7953
Bao Nhat <baonhat20@gmail.com>
2022-10-15 18:44:21 +0700

    lan2

--- forgot-password.php
+++ forgot-password.php
@@ -39,7 +39,7 @@ if (isset($_POST['submit'])) {
                 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                 $mail->Username   = 'baonhat20@gmail.com';                     //SMTP username
-                $mail->Password   = 'izdfegzuskitqkii';                               //SMTP password
+                $mail->Password   = 'zyeapbgpjngpelti';                               //SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
